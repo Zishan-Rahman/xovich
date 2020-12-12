906a1a332139b3aa0c293024f9d804b8d0b37e77
Zishan Rahman <zishanfrahman@outlook.com>
2020-12-12 23:50:40 +0000

    Create Main Class
    Creating the Main class where the GUI will be written.
    
    The original file is kept for reference.

--- ivanovichFinal.py
+++ ivanovichFinal.py
@@ -1,1 +1,2 @@
-print("Hello,", ''.join(*[[input("what is your name?:")+" "]*2])[:-1]+"ovich")
+if __name__ == "__main__":
+    print("Hello,", ''.join(*[[input("what is your name?:")+" "]*2])[:-1]+"ovich")
---
+++ main.py
@@ -0,0 +1,15 @@
+from tkinter import *
+
+class Main(Frame):
+    """
+    The GUI
+    """
+    
+    def __init__(self):
+        super(Main, self).__init__()
+    
+    #pass
+
+"""TODO
+Create the GUI
+Implement the Ivanovich program in Tkinter"""
