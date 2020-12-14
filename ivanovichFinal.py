030f75cd75ffaa63170649fd4b983ef329b4edbf
Zishan Rahman <zishanfrahman@outlook.com>
2020-12-14 18:12:22 +0000

    Added a few more comments to ivanovichFinal.py
    Primarily for reStructuredText documentation purposes.

--- ivanovichFinal.py
+++ ivanovichFinal.py
@@ -4,7 +4,9 @@
 # not in one line anymore, ha!
 
 def originalXovich():
+    """The original \"ovich\" line of code, now placed in its own function!"""
     print("Hello,", ''.join(*[[input("what is your name?:")+" "]*2])[:-1]+"ovich")
 
 if __name__ == "__main__":
+    """Runs the original line of code (once) when the file is run from the command line."""
     originalXovich()
